23d373eef26b3fb5fffb3b6f354c49a12b2f31d3
codedghost2 <codedghost2@gmail.com>
2019-11-13 22:39:03 +0000

    Initial Commit

---
+++ CoreCodedChatbot.Logging/CoreCodedChatbot.Logging/ErrorHandlingMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace CoreCodedChatbot.Logging
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context, ILogger<ErrorHandlingMiddleware> logger)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception e)
+            {
+                logger.LogCritical(e, $"Completely unhandled error encountered");
+                throw;
+            }
+        }
+    }
+}
---
+++ CoreCodedChatbot.Logging/CoreCodedChatbot.Logging/Package.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CoreCodedChatbot.Secrets;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Common;
+using NLog.Config;
+using NLog.Layouts;
+using NLog.Targets;
+using NLog.Web;
+
+namespace CoreCodedChatbot.Logging
+{
+    public static class Package
+    {
+        public static IServiceCollection AddChatbotNLog(this IServiceCollection services, ISecretService secretService)
+        {
+            services.AddLogging(builder =>
+            {
+                builder.ClearProviders();
+                builder.SetMinimumLevel(LogLevel.Information);
+                var config = new LoggingConfiguration();
+                var dbTarget = new DatabaseTarget("dbLog")
+                {
+                    ConnectionString = new SimpleLayout(secretService.GetSecret<string>("DbConnectionString"), ConfigurationItemFactory.Default),
+                    CommandText =
+                        "INSERT INTO LogEntry(Level, LoggedAt, Message, Logger, Callsite, Exception, StackTrace, ProcessName) " +
+                        "VALUES(@Level, @LoggedAt, @Message, @Logger, @Callsite, @Exception, @StackTrace, @ProcessName);",
+                    Parameters =
+                    {
+                        new DatabaseParameterInfo
+                        {
+                            Name = "@Level",
+                            Layout = new SimpleLayout("${level}")
+                        },
+                        new DatabaseParameterInfo
+                        {
+                            Name = "@LoggedAt",
+                            Layout = new SimpleLayout("${longdate}")
+                        },
+                        new DatabaseParameterInfo
+                        {
+                            Name = "@Message",
+                            Layout = new SimpleLayout("${message}")
+                        },
+                        new DatabaseParameterInfo
+                        {
+                            Name = "@Logger",
+                            Layout = new SimpleLayout("${logger}")
+                        },
+                        new DatabaseParameterInfo
+                        {
+                            Name = "@Callsite",
+                            Layout = new SimpleLayout("${callsite}")
+                        },
+                        new DatabaseParameterInfo
+                        {
+                            Name = "@Exception",
+                            Layout = new SimpleLayout("${exception}")
+                        },
+                        new DatabaseParameterInfo
+                        {
+                            Name = "@StackTrace",
+                            Layout = new SimpleLayout("${stacktrace}")
+                        },
+                        new DatabaseParameterInfo
+                        {
+                            Name = "@ProcessName",
+                            Layout = new SimpleLayout("${processname}")
+                        }
+                    },
+                    DBProvider = "Microsoft.Data.SqlClient.SqlConnection, Microsoft.Data.SqlClient"
+                };
+
+                InternalLogger.LogToConsoleError = true;
+
+                config.AddTarget(dbTarget);
+                config.AddRule(NLog.LogLevel.Warn, NLog.LogLevel.Fatal, "dbLog", "*");
+
+                var fileTarget = new ConsoleTarget("console");
+                config.AddTarget(fileTarget);
+                config.AddRule(NLog.LogLevel.Warn, NLog.LogLevel.Fatal, "console", "*");
+
+                builder.AddNLog(config);
+            });
+
+            return services;
+        }
+
+        public static void ConfigureNLogForConsoleApp<T>(IServiceProvider serviceProvider)
+        {
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler((sender, eventArgs) => UnhandledExceptionHandler<T>(sender, eventArgs, serviceProvider));
+        }
+
+        private static void UnhandledExceptionHandler<T>(object sender, UnhandledExceptionEventArgs eventArgs,
+            IServiceProvider serviceProvider)
+        {
+            var logger = serviceProvider.GetService<ILogger<T>>();
+
+            logger.LogError((Exception)eventArgs.ExceptionObject, $"Unhandled error encountered - Terminating: {eventArgs.IsTerminating}");
+        }
+    }
+}
